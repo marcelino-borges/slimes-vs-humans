257479e072cf77994362d248865e30d3a6582447
Rodrigo Silva <rodrigosilva.tec@hotmail.com>
2021-07-04 18:36:28 -0300

    SlimeProgress

---
+++ Assets/Scripts/SlimeData/SlimeBomb.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SlimeBomb : SlimeBase
+{
+    [Space(10f)]
+    [Header("Expecific Attributes")]
+    [SerializeField] private string _details;
+
+    private void OnMouseDown() {
+        Debug.Log("Click");
+        CloneItSelf(this.gameObject, true);
+    }
+    
+}
---
+++ Assets/Scripts/SlimeData/SlimeCollector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SlimeCollector : SlimeBase
+{
+    [Space(10f)]
+    [Header("Expecific Attributes")]
+    [SerializeField] private string _details;
+    [SerializeField] private Transform _target;
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        _navMeshAgent.SetDestination(_target.position);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Humans")
+        {
+            print("human");
+            Explode();
+        }
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("Click");
+        CloneItSelf(this.gameObject, true);
+    }
+}
