a8cfad3599bea397688313d0c3a0e4906b6a3162
Tiago Somogy Bou Rizk <35984005+Somogy@users.noreply.github.com>
2021-07-14 19:55:25 -0300

    Splat Effect in Humans

--- Assets/Scripts/VFX/SplatEffect.cs
+++ Assets/Scripts/VFX/SplatEffect.cs
@@ -6,13 +6,16 @@ public class SplatEffect : MonoBehaviour
 {
     [Header("List of splat prefab of each slimes")]
     [SerializeField] private List<GameObject> splatsDecal;
+
+    [Space(5)]
+    [Header("Color to use in splats")]
     [SerializeField] private List<Color> splatColor;
 
     [Space(5)]
-    [Tooltip("Interaction layers for splashback ")]
+    [Tooltip("Interaction layers for splashback")]
     [SerializeField] private LayerMask layersToCheck;
 
-    private bool canSplat;
+    private bool canSplat; // To count down time
 
     private ContactPoint slimeContactPoint;
     private GameObject selectedSplash;
@@ -45,7 +48,6 @@ public class SplatEffect : MonoBehaviour
                 selectedSplash.GetComponent<Renderer>().material.SetColor("_Color", splatColor[2]);
             }
 
-            //Instantiate(selectedSplash, collision.contacts[0].point, Quaternion.LookRotation(Vector3.up, slimeContactPoint.normal));
             canSplat = true;
 
             StartCoroutine(SplatEffectCD());
