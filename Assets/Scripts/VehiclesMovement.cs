c61feaa2fa94132e681046459b66115f118c23ac
Tiago Somogy Bou Rizk <35984005+Somogy@users.noreply.github.com>
2021-07-22 09:58:21 -0300

    Vehicles movement

--- Assets/Scripts/VehiclesMovement.cs
+++ Assets/Scripts/VehiclesMovement.cs
@@ -8,8 +8,16 @@ public class VehiclesMovement : MonoBehaviour
     [SerializeField] private Transform destinyPoint;
 
     private Vector3 vectorDestiny;
+    private Vector3 originalPosition;
 
-    private void Start() => vectorDestiny = new Vector3(destinyPoint.localPosition.x, destinyPoint.localPosition.y, destinyPoint.localPosition.z);
+    private void Start()
+    {
+        originalPosition = transform.localPosition;
+        vectorDestiny = new Vector3(destinyPoint.localPosition.x, destinyPoint.localPosition.y, destinyPoint.localPosition.z);
+    }
 
-    private void FixedUpdate() => transform.localPosition = Vector3.MoveTowards(transform.localPosition, vectorDestiny, velocity);
+    private void FixedUpdate()
+    {
+        transform.localPosition = Vector3.Lerp(originalPosition, originalPosition + vectorDestiny, Mathf.PingPong(Time.time, 1));
+    }
 }
